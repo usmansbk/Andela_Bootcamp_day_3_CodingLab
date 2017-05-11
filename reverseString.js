9980bff362997e6f4bdaddea393fcd09206ba94b
Usman Suleiman <usmansbk@gmail.com>
2017-05-11 11:47:06 +0100

    pass all test

--- reverseString.js
+++ reverseString.js
@@ -1,19 +1,28 @@
 /*
 function reverseString(string) {
+	if (string === '') return null;
 	let temp = "";
 	let len = string.length;
 	for (let counter = len-1; counter > 0; counter--) {
 		temp += string[counter];
 	}
-	return (temp === string)?true:false;
+	return (temp === string)?true:temp;
 }
 */
 
 function reverseString(string) {
-	for (let counter = 0; counter < string.length/2; counter++) {
-		if (string[counter] !== string[string.length - counter - 1])
-			return false;
+	if (string === '') return null;
+	let length = string.length;
+	for (let counter = 0; counter < length/2; counter++) {
+		if (string[counter] !== string[string.length - counter - 1]) {
+			let temp = '';
+			for (let counter = length; counter > 0; counter--) {
+				temp += string[counter-1];
+			}
+			return temp;
+		}
 	}
 	return true;
 }
+
 module.exports = reverseString;
